b866fbc0abf905dfc124a75f6a9003d56fbe9e99
loginweb <you@example.com>
2022-10-12 14:20:48 -0400

    update sms

--- index.ino
+++ index.ino
@@ -42,6 +42,9 @@ const char server[]   = "igps.live";
 #include <WiFi.h>
 #include <HTTPClient.h>
 
+#include <Preferences.h>
+Preferences preferences;
+
 #ifdef DUMP_AT_COMMANDS
 #include <StreamDebugger.h>
 StreamDebugger debugger(SerialAT, SerialMon);
@@ -94,10 +97,19 @@ void modemRestart()
     modemPowerOn();
 }
 
+
+//Variables globales
+float milat,  milon, mispeed, mialt, miaccuracy;
+int vsat2, usat2;
+long mitiempo = 3000; //seg
+int geocerca = 3;  //kmph
+String res = "";
+int velocidad_max = 0;
+String num_cliente = "";
 void setup() {
   // Set console baud rate
   SerialMon.begin(115200);
-  delay(500);
+  delay(1000);
 
   SerialAT.begin(9600, SERIAL_8N1, 26, 27);
   modemPowerOn();
@@ -127,9 +139,8 @@ void setup() {
     modem.sendAT("+CNMI=1,2,0,0");
     modem.waitResponse(1000L, response);
     Serial.println(response);
-    delay(5000);
+    delay(1000);
     
- 
   #if TINY_GSM_USE_GPRS
     // GPRS connection parameters are usually set after network registration
     SerialMon.print(F("Connecting to "));
@@ -161,19 +172,31 @@ void setup() {
      Serial.println(" CONNECTED");
      set_setting_wifi(modem.getIMEI());
   #endif  
-  
+
+  // Variables Globales en Memoria
+  Serial.println("/**********************************************************/");
+  Serial.println("Imprimiendo variables globales...");
+  Serial.println("/**********************************************************/");
+  preferences.begin("vgm", false);  
+  //  preferences.clear();
+  Serial.println("Propietario: " + preferences.getString("name_cliente", "user"));
+  Serial.println("Numero Prop: " + preferences.getString("num_cliente","72826861"));
+  Serial.println("Numero GPS: " + preferences.getString("num_gps", "67353115"));
+  Serial.println("Velocidad Max: " + String(preferences.getInt("velocidad_max", 40)));
+  Serial.println("Tiempo BR: " + String(preferences.getInt("mitiempo", 3000)));
+  Serial.println("Geo Cerca: " + String(preferences.getInt("geocerca", 3)));
+  Serial.println("GRPS: " + String(preferences.getBool("gprs", true)));
+  Serial.println("APN MOVIL: " + preferences.getString("apn", "4g.entel"));
+  Serial.println("Wifi SSID: " + preferences.getString("wifi_ssid", "admin"));
+  Serial.println("Wifi Pass: " + preferences.getString("wifi_pass", "password"));
+  velocidad_max = preferences.getInt("velocidad_max");
+  num_cliente = preferences.getString("num_cliente");
+  delay(1000);
+  preferences.end();
+  Serial.println();
+
 }
 
-//Variables globales
-float milat,  milon, mispeed, mialt, miaccuracy;
-int vsat2, usat2;
-long mitiempo = 3000; //seg
-int geocerca = 3;  //kmph
-int velocidad_max = 40;
-String num_cliente = "72823861";
-String num_gps = "67353115";
-String name_cliente = "Ing. Percy Alvarez";
-String res = "";
 void loop() {
   while (SerialAT.available()) {
     SerialMon.write(SerialAT.read());
@@ -182,8 +205,6 @@ void loop() {
      res = "";
      mymessage.toUpperCase();
              if(mymessage.indexOf("MAPA")>=0){              
-              Serial.println("Empezar a posicionar. Asegúrese de ubicar al aire libre.");
-              Serial.println("La luz indicadora azul parpadea para indicar el posicionamiento."); 
               while (1) {
                   if (modem.getGPS(&milat, &milon)) {
                     String mapa = "https://maps.google.com/maps?q=loc:"+String(milat, 8)+","+String(milon, 8);
@@ -208,7 +229,16 @@ void loop() {
               String imei = modem.getIMEI();
               String ccid = modem.getSimCCID();
               String cop = modem.getOperator();
-              res = modem.sendSMS("+591"+phoneint, "Imei: "+imei+"\nOPERADOR: "+cop+"\nPropietario: "+name_cliente+"\nNumero Prop: "+num_cliente+"\nNumero GPS: "+num_gps+"\nVel. Max.: "+velocidad_max);
+              preferences.begin("vgm", false);  
+              String adminname = preferences.getString("name_cliente");
+              String adminphone = preferences.getString("num_cliente");
+              String gpsphone = preferences.getString("num_gps");
+              int velmax = preferences.getInt("velocidad_max");
+              delay(1000);
+              preferences.end();
+              String mitext = "Imei: "+imei+"\nOPERADOR: "+cop+"\nPropietario: "+adminname+"\nNumero Prop: "+adminphone+"\nNumero GPS: "+gpsphone+"\nVel. Max. KM: "+String(velmax); 
+              Serial.println(mitext);
+              res = modem.sendSMS("+591"+phoneint, mitext);
                Serial.println("/**********************************************************/");
               if(res == "1"){
                 Serial.println("Mensaje enviado a "+phoneint);
@@ -218,7 +248,7 @@ void loop() {
                Serial.println("/**********************************************************/");
             }else if(mymessage.indexOf("GPS")>=0){
               while (1) {
-                if (modem.getGPS(&milat, &milon)) {                
+                if (modem.getGPS(&milat, &milon, &mispeed, &mialt, &vsat2, &usat2, &miaccuracy)) {                
                   String gps_raw = modem.getGPSraw();
                   Serial.println(gps_raw);
                   if (gps_raw != "") {
@@ -237,12 +267,94 @@ void loop() {
                   delay(500);
               }
               digitalWrite(LED_PIN, LOW);
-            }else if(mymessage.indexOf("LED")>=0){
+            }else if(mymessage.indexOf("SWITCH")>=0){
               digitalWrite(LED_PIN, !digitalRead(LED_PIN));
               Serial.println(digitalRead(LED_PIN));
+              res = modem.sendSMS("+591"+phoneint, "Switch Cambiado Exitosamente : "+String(digitalRead(LED_PIN)));
+               Serial.println("/**********************************************************/");
+              if(res == "1"){
+                Serial.println("Mensaje enviado a "+phoneint);
+              }else{
+                   Serial.println("Mensaje NO enviado ERROR a :"+ phoneint);
+              }
+               Serial.println("/**********************************************************/");
+               break;
+
+             }else if(mymessage.indexOf("ADMIN NAME")>=0){ //largo total 41 + 13 = 54 
+              String text_name = mymessage.substring(54, mymessage.length());
+              Serial.println(String(mymessage));
+              Serial.println(String(mymessage.length()));
+              text_name.trim();
+              Serial.println(text_name);
+              res = modem.sendSMS("+591"+phoneint, "Admin name Actualizado a "+text_name);
+              preferences.begin("vgm", false);  
+              preferences.putString("name_cliente", text_name);
+              preferences.end();
+              Serial.println("/**********************************************************/");
+              if(res == "1"){
+                Serial.println("Mensaje enviado a "+phoneint);
+              }else{
+                   Serial.println("Mensaje NO enviado ERROR a :"+ phoneint);
+              }
+               Serial.println("/**********************************************************/");
+               break;    
+             }else if(mymessage.indexOf("ADMIN PHONE")>=0){ //largo total 41 + 14 = 55 
+              String text = mymessage.substring(55, mymessage.length());
+              Serial.println(String(mymessage));
+              Serial.println(String(mymessage.length()));
+              text.trim();
+              Serial.println(text);
+              res = modem.sendSMS("+591"+phoneint, "Admin phone Actualizado a "+text);
+              preferences.begin("vgm", false);  
+              preferences.putString("num_cliente", text);
+              preferences.end();
+              Serial.println("/**********************************************************/");
+              if(res == "1"){
+                Serial.println("Mensaje enviado a "+phoneint);
+              }else{
+                   Serial.println("Mensaje NO enviado ERROR a :"+ phoneint);
+              }
+               Serial.println("/**********************************************************/");
+               break;   
+             }else if(mymessage.indexOf("GPS PHONE")>=0){ //largo total 41 + 12 = 53 
+              String text = mymessage.substring(53, mymessage.length());
+              Serial.println(String(mymessage));
+              Serial.println(String(mymessage.length()));
+              text.trim();
+              Serial.println(text);
+              res = modem.sendSMS("+591"+phoneint, "Gps phone Actualizado a "+text);
+              preferences.begin("vgm", false);  
+              preferences.putString("num_cliente", text);
+              preferences.end();
+              Serial.println("/**********************************************************/");
+              if(res == "1"){
+                Serial.println("Mensaje enviado a "+phoneint);
+              }else{
+                   Serial.println("Mensaje NO enviado ERROR a :"+ phoneint);
+              }
+               Serial.println("/**********************************************************/");
+               break;   
+             }else if(mymessage.indexOf("VELOCIDAD MAX")>=0){ //largo total 41 + 16 = 57 
+              String text = mymessage.substring(57, mymessage.length());
+              Serial.println(String(mymessage));
+              Serial.println(String(mymessage.length()));
+              text.trim();
+              Serial.println(text);
+              res = modem.sendSMS("+591"+phoneint, "Veocidad Max Actualizado a "+text);
+              preferences.begin("vgm", false);  
+              preferences.putString("num_cliente", text);
+              preferences.end();
+              Serial.println("/**********************************************************/");
+              if(res == "1"){
+                Serial.println("Mensaje enviado a "+phoneint);
+              }else{
+                   Serial.println("Mensaje NO enviado ERROR a :"+ phoneint);
+              }
+               Serial.println("/**********************************************************/");
+               break;             
             }else{
-              Serial.println("No envio ninguna palabra clave, intenta con las siguientes palabras:\n1.- Info\n2.- Mapa\n3.- Gps\n4.- Led");
-              res = modem.sendSMS("+591"+phoneint, "No envio ninguna palabra clave, intenta con las siguientes palabras:\n1.- Info\n2.- Mapa\n3.- Gps\n4.- Switch, \n5.- Mas");
+              Serial.println("No envio ninguna palabra clave, intenta con las siguientes palabras:\n1.- Info\n2.- Mapa\n3.- Gps\n4.- switch");
+              res = modem.sendSMS("+591"+phoneint, "No envio ninguna palabra clave, intenta con las siguientes palabras:\n1.- Info\n2.- Mapa\n3.- Gps\n4.- Switch");
                Serial.println("/**********************************************************/");
               if(res == "1"){
                 Serial.println("Mensaje enviado a "+phoneint);
@@ -256,8 +368,6 @@ void loop() {
   while (SerialMon.available()) {
       SerialAT.write(SerialMon.read());
   }
-    Serial.println("Empezar a posicionar. Asegúrese de ubicar al aire libre.");
-    Serial.println("La luz indicadora azul parpadea para indicar el posicionamiento."); 
     enableGPS();
     while (1) {
         if (modem.getGPS(&milat, &milon, &mispeed, &mialt, &vsat2, &usat2, &miaccuracy)) {
@@ -269,16 +379,15 @@ void loop() {
             Serial.print("Visible Satellites: "); Serial.println(vsat2+" Used Satellites: "+usat2);
             Serial.print("Senal:"); Serial.println(miaccuracy, 8);
             String gps_raw = modem.getGPSraw();
-            Serial.println(gps_raw);
+            Serial.println(gps_raw);            
             if(mispeed > geocerca){
                 #if TINY_GSM_USE_WIFI
-                  
                 #endif                  
                 #if TINY_GSM_USE_GPRS
                   enviar_punto(modem.getIMEI(), milat, milon, mispeed, mialt);
+                  enviar_socket(modem.getIMEI(), milat, milon, mispeed, mialt);
                 #endif  
             }
-
               if(mispeed > velocidad_max ){
                res = modem.sendSMS("+591"+num_cliente, "Mensaje de alerte, supero la velocidad maxima establecia cual es: "+String(mispeed));
                 if(res == "1"){
@@ -309,8 +418,8 @@ void set_setting_wifi(String imei){
 
       mitiempo = doc["delay"];
       geocerca = doc["geocerca"];
-      velocidad_max = doc["velocidad_max"];
-      Serial.println(mitiempo + geocerca + velocidad_max);
+//      velocidad_max = doc["velocidad_max"];
+//      Serial.println(mitiempo + geocerca + velocidad_max);
       
     } else {
       Serial.print("err:");
@@ -340,7 +449,7 @@ void enviar_punto(String imei, float latitud, float longitud, float velocidad, f
     delay(10000);
     return;
   }
-  SerialMon.println(" success");
+  SerialMon.println("success a "+String(server));
   String miresource = "/api/waypoints/save?imei="+imei+"&lat="+String(latitud, 8)+"&long="+String(longitud, 8)+"&speed="+velocidad+"&height="+altura;
   Serial.println(miresource);
   SerialMon.println("Performing HTTP POST request...");
@@ -355,6 +464,34 @@ void enviar_punto(String imei, float latitud, float longitud, float velocidad, f
       char c = client.read();
       SerialMon.print(c);
       timeout = millis();
+    }
+  }
+  SerialMon.println();
+  client.stop();
+  SerialMon.println(F("Server disconnected"));
+}
+
+void enviar_socket(String imei, float latitud, float longitud, float velocidad, float altura){
+  SerialMon.println("Connecting to socket.igps.live");
+  if (!client.connect("socket.igps.live", 80)) {
+    SerialMon.println(" fail");
+    delay(10000);
+    return;
+  }
+  SerialMon.println("success a socket.igps.live");
+  String miresource2 = "/socket?imei="+imei+"&lat="+String(latitud, 8)+"&long="+String(longitud, 8)+"&speed="+velocidad;
+  SerialMon.println("Performing HTTP POST request...");
+  client.print(String("POST ") + miresource2 + " HTTP/1.1\r\n");
+  client.print("Host: socket.igps.live\r\n");
+  client.print("Content-Type application/json\r\n");
+  client.print("Connection: close\r\n\r\n");
+  client.println();
+  uint32_t timeout2 = millis();
+  while (client.connected() && millis() - timeout2 < 10000L) {
+    while (client.available()) {
+      char c = client.read();
+      SerialMon.print(c);
+      timeout2 = millis();
     }
   }
   SerialMon.println();
